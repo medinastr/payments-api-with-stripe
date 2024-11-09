761b6380032247f1a2f9be67cfb4860b85a13c8e
medinastr <pedromstracieri@gmail.com>
2024-11-09 17:14:36 -0300

    adding spring initializr project and application.yml

---
+++ payments/src/test/java/com/medinastr/payments/PaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medinastr.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
