4cc00fad1ea60c585ed5ac04accd2f0a8340c73d
medinastr <pedromstracieri@gmail.com>
2024-11-15 11:52:04 -0300

    adding products value object class

---
+++ payments/src/main/java/com/medinastr/payments/model/dto/ProductsDTO.java
@@ -0,0 +1,18 @@
+package com.medinastr.payments.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductsDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Float price;
+}
