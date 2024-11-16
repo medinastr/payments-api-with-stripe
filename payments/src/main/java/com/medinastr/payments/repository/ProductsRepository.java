5c8e9bf32e270f69d086602968920784af241f1f
medinastr <pedromstracieri@gmail.com>
2024-11-15 22:03:05 -0300

    adding methods to repository

--- payments/src/main/java/com/medinastr/payments/repository/ProductsRepository.java
+++ payments/src/main/java/com/medinastr/payments/repository/ProductsRepository.java
@@ -2,8 +2,16 @@ package com.medinastr.payments.repository;
 
 import com.medinastr.payments.model.entity.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductsRepository extends JpaRepository<Products, Long> {
+
+    @Query(value = "SELECT p FROM Products p WHERE p.price < :price")
+    List<Products> findLowPrices(Float price);
+
+    List<Products> findAllPricesLessThan(Float price);
 }
