76b3105bef748e23b700fbeb3623dfec90a3c234
medinastr <pedromstracieri@gmail.com>
2024-11-15 21:55:37 -0300

    adding products repository interface

---
+++ payments/src/main/java/com/medinastr/payments/repository/ProductsRepository.java
@@ -0,0 +1,9 @@
+package com.medinastr.payments.repository;
+
+import com.medinastr.payments.model.entity.Products;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductsRepository extends JpaRepository<Products, Long> {
+}
